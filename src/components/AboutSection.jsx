94f953ded2a4c9391af141b040dfbcc23ea41281
sksohaib147 <91631829+sksohaib147@users.noreply.github.com>
2025-10-19 22:35:45 +0500

    up

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -13,9 +13,32 @@ export const AboutSection = () => {
                 <div className=" grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
 
                     <div className="space-y-6">
-                        <h3>
-                            Passionate about building user-friendly and efficient web applications.
+                        <h3 className="text-2xl font-semibold" >
+                            Web Developer
                         </h3>
+
+                        <p className="text-muted-foreground">
+                            Passionate about building user-friendly and efficient web applications
+                            
+                            </p>
+
+                            <p className="text-muted-foreground">
+                            Passionate about building user-friendly and efficient web applications
+                            
+                            </p>
+
+                            <div className="flex flex-col sm:flex-row  gap-4 pt-4 justify-center ">
+
+
+                            <a href ="#contact" className="cosmic-button"> {" "}Get in touch </a>
+
+
+                                <a href ="#contact" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"> {" "}Download CV
+
+
+                                </a>
+
+                            </div>
 
                     </div>
 
