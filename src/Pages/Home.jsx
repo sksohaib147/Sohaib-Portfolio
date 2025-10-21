c2e4838530698aad5dc93477b880663c2abb461b
sksohaib147 <91631829+sksohaib147@users.noreply.github.com>
2025-10-21 16:28:35 +0500

    push

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
 
 export const Home = () => {
 
@@ -29,6 +30,7 @@ export const Home = () => {
                 <HeroSection />
                 <AboutSection />
                 <SkillsSection />
+                <ProjectsSection />
             </main>
 
 
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        id: 1,
+        title: "Project 1",
+        description: "Description of Project 1",
+        image: "/Projects/project1.png",
+
+    }
+]
+
+
+export default function ProjectsSection() {
+  return (
+    <section>
+
+    </section>
+  );
+}
